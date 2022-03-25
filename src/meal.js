ee556b4c2d6bcc5954f457912c137b074f4d584d
Jordan Farelli <jordanfarelli@gmail.com>
2022-03-24 20:41:11 -0600

    Completed meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,55 @@
-function nameMenuItem() {
+function nameMenuItem(string) {
+  return `Delicious ${string}`;
+}
 
+
+
+function createMenuItem(itemName, itemPrice, itemType) {
+    return {
+      name: itemName,
+      price: itemPrice,
+      type: itemType,
+    }
+}
+
+
+
+function addIngredients(string, ingredients) {
+  if (ingredients.includes(string)) {
+    return ingredients;
+  } else {
+    ingredients.push(string);
+  }
+}
+
+
+
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+
+
+function decreasePrice(price) {
+  return price * 0.90
+}
+
+
+
+function createRecipe(itemTitle, itemIngredients, itemType) {
+  return {
+    title: itemTitle,
+    ingredients: itemIngredients,
+    type: itemType,
+  }
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
